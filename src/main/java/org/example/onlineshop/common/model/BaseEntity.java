7275bb5a2d7890a4de7a6a2ea9be10211b279c8b
samaneh <meftahisamaneh1996@gmail.com>
2025-11-01 21:31:23 +0330

    refactor(common): enhance BaseEntity with auditing, soft delete, and optimistic locking

--- src/main/java/org/example/onlineshop/common/model/BaseEntity.java
+++ src/main/java/org/example/onlineshop/common/model/BaseEntity.java
@@ -2,20 +2,64 @@ package org.example.onlineshop.common.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(org.springframework.data.jpa.domain.support.AuditingEntityListener.class)
+@Where(clause = "is_deleted = false")
 @Getter
 @Setter
-public abstract class BaseEntity {
+@NoArgsConstructor
+@SuperBuilder
+public abstract class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
     private Long id;
-    private LocalDateTime createdAt = LocalDateTime.now();
-    private LocalDateTime updatedAt = LocalDateTime.now();
 
-    @PreUpdate
-    public void preUpdate() { updatedAt = LocalDateTime.now(); }
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updateAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false, length = 100)
+    private Long createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+
+    @Version
+    private Long version;
+
+
+    public void markDeleted() {
+        this.isDeleted = true;
+    }
+
+    public boolean isActive() {
+        return !this.isDeleted;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "(id=" + id + ")";
+    }
 }
